8c7a015d48d959d805795ca0b2b99c0b3e961737
Brayan_98 <bryaux98@gmail.com>
2024-05-24 23:01:26 -0500

    ajustes-reporte-entrega-elementos

--- resources/views/entregas_elementos/reporte.blade.php
+++ resources/views/entregas_elementos/reporte.blade.php
@@ -5,31 +5,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reporte Entrega de Elementos</title>
+    <!-- AdminLTE App -->
     <link rel="stylesheet" href="{{ asset('adminlte/css/adminlte.css') }}">
+    <style>
+        .firma {
+            display: inline-block;
+            width: 200px;
+            border-bottom: 2px solid #000000;
+        }
+    </style>
 </head>
 <body>
-    <x-text size="h4" color="primary" :value="__('Entrega de Elementos')" class="text-center mb-4" />
+    <div class="mb-5">
+        <h4 class="font-weight-bold text-primary d-inline">Entrega de Elementos</h4>
+        <h5 class="font-weight-bold text-primary d-inline float-right">#{{ $entrega_elemento->id_entrega_elemento }}</h5>
+    </div>
 
-    <x-text size="h6" color="black" :value="__('Proyecto')" />
-    <x-text size="h6" style="font-weight-normal" color="black" :value="$proyecto->proyecto" />
+    <div class="d-block">
+        <h6 class="font-weight-bold d-inline">Proyecto:</h6>
+        <h6 class="font-weight-normal d-inline ml-1">{{ $proyecto->proyecto }}</h6>
+    </div>
 
-    <x-text size="h6" color="black" :value="__('Empleado')" class="mt-4" />
-    <x-text size="h6" style="font-weight-normal" color="black" :value="$entrega_elemento->empleado->empleado" />
+    <div class="d-block">
+        <h6 class="font-weight-bold d-inline">Empleado:</h6>
+        <h6 class="font-weight-normal d-inline ml-1">{{ $entrega_elemento->empleado->empleado }}</h6>
+    </div>
 
-    <x-text size="h6" color="black" :value="__('Fecha de Entrega')" class="mt-4" />
-    <x-text size="h6" style="font-weight-normal" color="black" :value="$entrega_elemento->fecha_entrega" />
+    <div class="d-block">
+        <h6 class="font-weight-bold d-inline">Fecha de Entrega:</h6>
+        <h6 class="font-weight-normal d-inline ml-1">{{ $entrega_elemento->fecha_entrega }}</h6>
+    </div>
 
-    <x-data-table :headers="['#', 'Elemento', 'Marca', 'Modelo', 'Tipo de Cantidad', 'Cantidad Entregada']" class="mt-4">
-        @foreach ($detalle_entrega_elementos as $detalle_entrega_elemento)
+    <table class="table mt-5">
+        <thead class="bg-primary text-white">
             <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $detalle_entrega_elemento->elemento->item->item }}</td>
-                <td>{{ $detalle_entrega_elemento->elemento->marca }}</td>
-                <td>{{ $detalle_entrega_elemento->elemento->modelo }}</td>
-                <td>{{ $detalle_entrega_elemento->elemento->tipoCantidad->tipo_cantidad }}</td>
-                <td>{{ $detalle_entrega_elemento->cantidad }}</td>
+                <th>#</th>
+                <th>Elemento</th>
+                <th>Marca</th>
+                <th>Modelo</th>
+                <th>Tipo de Cantidad</th>
+                <th>Cantidad Entregada</th>
             </tr>
-        @endforeach
-    </x-data-table>
+        </thead>
+        <tbody>
+            @foreach ($detalle_entrega_elementos as $detalle_entrega_elemento)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $detalle_entrega_elemento->elemento->item->item }}</td>
+                    <td>{{ $detalle_entrega_elemento->elemento->marca }}</td>
+                    <td>{{ $detalle_entrega_elemento->elemento->modelo }}</td>
+                    <td>{{ $detalle_entrega_elemento->elemento->tipoCantidad->tipo_cantidad }}</td>
+                    <td>{{ $detalle_entrega_elemento->cantidad }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="text-center mt-5">
+        <div class="firma mt-5"></div>
+        <h6 class="font-weight-bold">Firma Empleado</h6>
+    </div>
 </body>
 </html>
